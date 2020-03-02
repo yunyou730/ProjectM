8d4e6d98d3fdb8a908d5c83a2d75522990efaeec
miaoyun <502765299@qq.com>
2020-03-02 10:23:33 +0800

    +增加  Initializer 控制游戏流程。考虑重命名为 GameManager +增加 Menu Home ,尝试模仿 war3 制作 首页 MenuHome , 局域网界面  MenuWorld,  主机界面 MenuLobby， 完成 首页 -> 局域网游戏 -> 游戏准备界面 的流程

---
+++ RPG/Assets/_Scripts/UI/View/MenuHome.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHome : MonoBehaviour
+{
+    public GameObject lanGamePrefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnClickLanGame()
+    {
+
+    }
+
+    public void OnclickQuit()
+    {
+        Application.Quit();
+    }
+}
