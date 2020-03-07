75f191e6c7ccf6e6493879b4863edfe9bb858637
miaoyun <502765299@qq.com>
2020-03-07 18:21:45 +0800

    +增加  一些空的 菜单 跳转逻辑

---
+++ RPG/Assets/_Scripts/UI/View/MenuBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBase : MonoBehaviour
+{
+    public Dictionary<string,object> enterArg = null;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    /*
+    public void SetEnterArg(Dictionary<string, object> enterArg)
+    {
+        this.enterArg = enterArg;
+    }
+    */
+}
---
+++ RPG/Assets/_Scripts/UI/View/MenuLobby.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLobby : MenuBase
+{
+    bool bSelfHost = false;
+
+    private void Awake()
+    {
+        Debug.Log("awake..");    
+    }
+
+    void Start()
+    {
+        // enterArg has be set
+        bSelfHost = enterArg.ContainsKey("self_host");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnDestroy()
+    {
+        // server close notify host, disconnect all network session
+        // client disconnect network session
+    }
+
+    public void OnClickStart()
+    {
+        Debug.Log("Start");
+    }
+
+    public void OnClickQuit()
+    {
+        // close
+        CmdCenter.GetInstance().RunCmd(new CmdCloseMenu(gameObject));
+        // open parent
+        Dictionary<string, object> arg = new Dictionary<string, object>();
+        arg.Add("menu_path","Menu/MenuLanGame");
+        CmdCenter.GetInstance().RunCmd(new CmdOpenMenu(arg));
+    }
+}
